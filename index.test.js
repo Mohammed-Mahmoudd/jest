ef17d1afb165b7071caccb5a0624dd15e2053c7a
Mohammed-Mahmoudd <gamermada2@gmail.com>
2026-06-15 10:00:36 +0300

    Adding Calculator Integration Testing with Jest

--- index.test.js
+++ index.test.js
@@ -1,5 +1,8 @@
-const add = require("./add");
+const calculator = require("./calculator");
 
 test("adds 1 + 2 to equal 3", () => {
-  expect(add(1, 2)).toBe(3);
+  expect(calculator.add(1, 2)).toBe(3);
+});
+test("Integration Testing with add and mul", () => {
+  expect(calculator.add(calculator.mul(4, 5), 2)).toBe(22);
 });
